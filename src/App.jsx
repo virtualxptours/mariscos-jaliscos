2fc1f2555ba9286c2839f0daea894a8856258037
angel <angel.nievesalicea@virtualxptours.com>
2025-02-27 22:11:06 -0500

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          This is a test {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
